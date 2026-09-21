c74857a25198d162148e2b4d3f53a12d758f099f
Koala Yeung <koalay@gmail.com>
2026-09-21 18:11:42 +0800

    fix: ignore generated Lingui catalogs in lint

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist/', 'node_modules/', 'playwright-report/', 'test-results/'] },
+  { ignores: ['dist/', 'node_modules/', 'playwright-report/', 'test-results/', 'src/i18n/locales/*.js'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: { ecmaVersion: 'latest', parserOptions: { ecmaFeatures: { jsx: true } }, globals: { ...globals.browser, ...globals.node } },
